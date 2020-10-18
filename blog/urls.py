ab3fb93b26d383f523e8fcf250c8da4ce0a82eb4
Administrator <jaishivnani30@gmail.com>
2020-10-18 19:38:41 +0530

    Uploading whole repository again

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+
+urlpatterns = [
+    path("",views.index, name="BlogHome"),
+    path("blogpost/<int:id>",views.blogpost, name="blogHome"),
+    path("blogpost/<int:id>",views.blogpost, name="blogHome"),
+
+
+
+]
