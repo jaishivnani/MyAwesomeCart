6af239f5ad19b52558023a48bae1c461d67f584c
Jai Shivnani <jaishivnani30@gmail.com>
2020-10-19 23:25:20 +0530

    uploading again

--- shop/views.py
+++ shop/views.py
@@ -13,7 +13,7 @@ MERCHANT_KEY = 'Vz27mm5IxGCV#AGB';
 def index(request):
     allProds = []
     # Fetching the Category
-    catprods = Product.objects.values('category')
+    catprods = Product.objects.values('category','id')
     # Fetching the item's category for items linked with categories.
     cats = {item['category'] for item in catprods}
 
@@ -26,6 +26,32 @@ def index(request):
     params = {'allProds': allProds}
 
     return render(request, 'shop/index.html', params)
+
+def searchMatch(query, item):
+    '''return true only if query matches the item'''
+    query = query.lower()
+    if query in item.desc.lower() or query in item.product_name.lower() or query in item.category.lower():
+        return True
+    else:
+        return False
+
+def search(request):
+    query = request.GET.get('search')
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prodtemp = Product.objects.filter(category=cat)
+        prod = [item for item in prodtemp if searchMatch(query, item)]
+
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        if len(prod)!= 0:
+            allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds': allProds, "msg":""}
+    if len(allProds)==0 or len(query)<4:
+        params = {'msg': "Please make sure to enter relevant search query"}
+    return render(request, 'shop/search.html', params)
 
 
 def about(request):
@@ -64,12 +90,6 @@ def tracker(request):
             return HttpResponse('{}')
 
     return render(request, 'shop/tracker.html')
-
-
-def search(request):
-
-    return render(request, 'shop/search.html')
-
 
 def productView(request, myid):
     # Fetch the product using the id.
